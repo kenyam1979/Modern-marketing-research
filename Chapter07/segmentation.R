f68e04f428565bc7dc0964c901cf0303f68c50be
山路憲 <ken.yamaji@gmail.com>
2021-08-01 20:38:21 +0900

    20210801

--- Chapter07/segmentation.R
+++ Chapter07/segmentation.R
@@ -1,6 +1,7 @@
 library(tidyverse)
+library(GGally)
 
-# データ準備
+# データ準備 ----
 seg <- read_csv('http://yuhikaku-nibu.txt-nifty.com/blog/files/seg.txt', locale=locale(encoding="shift_jis"))
 seg <- seg %>% rename(age=年齢, sex=性別, expr_yr=投資経験年数, trading_type=取引形態)
 
@@ -16,11 +17,15 @@ seg %>% select(age) %>%
   ggplot(aes(age)) + 
   geom_histogram(binwidth=5)
 
-# 因子分析
+# 因子分析 ----
 ### (注意) scoreを指定しないと後で値を取れない
 fa <- factanal(~q1+q2+q3+q4+q5+q6+q7+q8+q9+q10+q11+q12+q13+q14+q15+q16+q17+q18+q19+q20+q21+q22+q23+q24+q25+q26+q27, 
                factors=4, data=seg, rotation='varimax', scores='regression')
+#fa <- factanal(~q1+q2+q3+q4+q5+q6+q7+q8+q9+q10+q11+q12+q13+q14+q15+q16+q17+q18+q19+q20+q21+q22+q23+q24+q25+q26+q27, 
+#               factors=6, data=seg, rotation='varimax', scores='regression')
 fa
+### Cumulative varが50.6%で十分説明力があるとしている
+### あるいはfactorを5や6にしても説明力はさほどあがらないので、factorを4としている
 
 ## ファクターの読み解き
 head(sort(fa$loadings[, 1], decreasing=TRUE), 5) # インターネット情報収集
@@ -29,24 +34,29 @@ head(sort(fa$loadings[, 3], decreasing=TRUE), 5) # 企業業績重視
 head(sort(fa$loadings[, 4], decreasing=TRUE), 5) # 知人、営業のススメ
 
 
-# k-meansでのセグメンテーション
+# k-meansでのセグメンテーション ----
 scores <- fa$scores
 km <- kmeans(scores, centers=4)
 
-## data_frame(f1=fa$scores[,1], f2=fa$scores[,2], cluster=as.factor(km$cluster)) %>%
-  ggplot(aes(x=f1, y=f2, color=cluster)) + geom_point()
+## 結果の可視化
+data_frame(data.frame(fa$scores), cluster=as.factor(km$cluster)) %>%
+  ggpairs(aes(color=cluster),
+          lower = list(continuous = wrap("points", size = 1.5, alpha = 0.8)),
+          upper = list(continuous = wrap("cor", size = 5)),
+          diag = list(continuous = wrap("densityDiag", alpha = 0.5)))
 
+## 結果のチェック
 km$size # 各サンプルが特定のクラスタに偏っていないか
 km$centers #どの軸に重みがあるか
 
+## 軸の重みの可視化
 data.frame(cluster=rownames(km$centers), km$centers) %>% 
   pivot_longer(cols=-cluster, names_to='factors') %>%
   ggplot() + 
-  geom_bar(stat='identity', aes(x=factors, y=value)) +
+  geom_bar(stat='identity', aes(x=factors, y=value, fill=cluster)) +
   facet_grid(cluster~.)
 
-
-# 結果分析
+# クラスタの属性分析 ----
 df <- tibble(panel=seg$panel, cluster=factor(km$cluster), seg[ ,29:32])  # 元データにクラスタと顧客属性を追加
 df$sex <- factor(df$sex)
 df$trading_type <- factor(df$trading_type) 
