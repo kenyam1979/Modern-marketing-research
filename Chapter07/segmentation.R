d09723d0657ea80ec1d6599419efc1914a98d81b
山路憲 <ken.yamaji@gmail.com>
2021-08-01 23:09:30 +0900

    20210801_2

--- Chapter07/segmentation.R
+++ Chapter07/segmentation.R
@@ -5,7 +5,7 @@ library(GGally)
 seg <- read_csv('http://yuhikaku-nibu.txt-nifty.com/blog/files/seg.txt', locale=locale(encoding="shift_jis"))
 seg <- seg %>% rename(age=年齢, sex=性別, expr_yr=投資経験年数, trading_type=取引形態)
 
-## seg <- seg %>% mutate(across(starts_with('q'), ~ .%/%3))
+# seg <- seg %>% mutate(across(starts_with('q'), ~ .%/%4))
 
 seg %>% 
   group_by(sex) %>% 
@@ -16,6 +16,9 @@ seg %>%
 seg %>% select(age) %>% 
   ggplot(aes(age)) + 
   geom_histogram(binwidth=5)
+
+
+
 
 # 因子分析 ----
 ### (注意) scoreを指定しないと後で値を取れない
@@ -33,10 +36,20 @@ head(sort(fa$loadings[, 2], decreasing=TRUE), 5) # マクロ安定志向
 head(sort(fa$loadings[, 3], decreasing=TRUE), 5) # 企業業績重視
 head(sort(fa$loadings[, 4], decreasing=TRUE), 5) # 知人、営業のススメ
 
+## ファクターの
+data_frame(q=formatC(1:27, width=2, flag=0), as_tibble(fa$loadings[])) %>%
+  pivot_longer(cols=-q, names_to='factors') %>%
+  ggplot() + 
+  geom_bar(stat='identity', aes(x=q, y=value, fill=factors)) +
+  coord_flip() +
+  facet_grid(.~factors)
+
+
+
 
 # k-meansでのセグメンテーション ----
 scores <- fa$scores
-km <- kmeans(scores, centers=4)
+km <- kmeans(scores, centers=3)
 
 ## 結果の可視化
 data_frame(data.frame(fa$scores), cluster=as.factor(km$cluster)) %>%
@@ -55,6 +68,10 @@ data.frame(cluster=rownames(km$centers), km$centers) %>%
   ggplot() + 
   geom_bar(stat='identity', aes(x=factors, y=value, fill=cluster)) +
   facet_grid(cluster~.)
+
+
+
+
 
 # クラスタの属性分析 ----
 df <- tibble(panel=seg$panel, cluster=factor(km$cluster), seg[ ,29:32])  # 元データにクラスタと顧客属性を追加
@@ -84,6 +101,16 @@ df %>%
   summarize(n=n()) %>%
   ggplot() + 
   geom_bar(aes(x=cluster, y=n, fill=trading_type), position='fill', stat='identity')
+
+
+
+
+
+# Appendix. クラスタ数の評価 ----
+library(NbClust)
+NbClust(scores, distance = "euclidean",
+        min.nc = 2, max.nc = 5, 
+        method = "ward.D2", index ="all")
 
 
 
